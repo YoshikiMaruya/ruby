51fc19a79c23ddbb4619fce286a448309f8fe8d9
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-04-06 02:53:52 +0900

    feat: created the new methods

--- class/dragon.rb
+++ class/dragon.rb
@@ -1,16 +1,14 @@
 #2021/03/17 YoshikiMaruya
 
 class Dragon
-  def initialize(hit_point, offensive_power, defense_power, fire_type)
+  def initialize(hit_point, offensive_power, defense_power)
     @hit_point = hit_point
     @offensive_power = offensive_power
     @defense_power = defense_power
-    @fire_type = fire_type
   end
 
   def spit_fire
-    @fire_type
-    @offensive_power
+    @offensive_power * 2.0
   end
 
   def shred
